130d6f40f5cb3ab6b2963690f29056f78f7aeea3
fbmnds <fbmnds7@gmail.com>
2014-06-08 20:27:19 +0200

    some code reorganisation

--- FFSync.fsx
+++ FFSync.fsx
@@ -77,34 +77,114 @@ module ObjectFormats =
                    predecessorid : string;
                    ``type``      : string }
 
-    type Folder = { title : string;
-                    parentid : string;
-                    parentName : string;
+    type Folder = { title         : string;
+                    parentid      : string;
+                    parentName    : string;
                     predecessorid : string;
-                    ``type`` : string }
+                    ``type``      : string }
 
-    type Livemark = { siteUri : string;
-                      feedUri : string;
-                      title : string;
-                      parentid : string;
-                      parentName : string;
+    type Livemark = { siteUri       : string;
+                      feedUri       : string;
+                      title         : string;
+                      parentid      : string;
+                      parentName    : string;
                       predecessorid : string;
-                      ``type`` : string }
+                      ``type``      : string }
 
-    type Separator = { pos : string;
-                       parentid : string;
-                       parentName : string;
+    type Separator = { pos           : string;
+                       parentid      : string;
+                       parentName    : string;
                        predecessorid : string;
-                       ``type`` : string; 
-                       children : string [] }
+                       ``type``      : string; 
+                       children      : string [] }
 
-    type Clients = { name : string;
-                     ``type`` : string;
-                     commands : string [];
-                     version : string option;
+    type Clients = { name      : string;
+                     ``type``  : string;
+                     commands  : string [];
+                     version   : string option;
                      protocols : string [] option }
 
-    type ClientsPayload = {  }
+    type ClientsPayloadCapabilities = string
+
+    type ClientsPayload = { name         : string;
+                            formfactor   : string;
+                            application  : string;
+                            version      : string;
+                            capabilities : ClientsPayloadCapabilities;
+                            mpEnabled    : Boolean }
+
+    type CommandsData = string
+
+    type Commands = { receiverID : string;
+                      senderID   : string;
+                      created    : Int64;
+                      action     : string;
+                      data       : CommandsData }
+
+    type Forms =  { name :  string;
+                    value : string }
+
+    type HistoryTransition = 
+    | TRANSITION_LINK  = 1
+    | TRANSITION_TYPED = 2
+    | TRANSITION_BOOKMARK = 3
+    | TRANSITION_EMBED = 4
+    | TRANSITION_REDIRECT_PERMANENT = 5
+    | TRANSITION_REDIRECT_TEMPORARY = 6
+    | TRANSITION_DOWNLOAD = 7
+    | TRANSITION_FRAMED_LINK = 8
+
+    type HistoryPayloadVisits = { uri    : string;
+                                  title  : string;
+                                  visits : string [] }
+
+    type HistoryPayload = { items : HistoryPayloadVisits }
+
+    type History = { histUri  : string;
+                     title    : string;
+                     visits   : HistoryPayload;
+                     date     : int; // datetime of the visit
+                     ``type`` : HistoryTransition }
+    
+    type Passwords = { hostname      : string;
+                       formSubmitURL : string;
+                       httpRealm     : string;
+                       username      : string;
+                       password      : string;
+                       usernameField : string;
+                       passwordField : string }
+
+    type Preferences = { value    : string;
+                         name     : string option;
+                         ``type`` : string option }
+
+    let x = { value = "value"; name = Some "name"; ``type`` = Some "type" }
+    
+    let x' = x.GetType() 
+    printfn "%A" x'
+
+    module TabsVersions =
+        
+        type StringOrInteger =
+        | String of string
+        | Integer of int 
+
+        type Version1 = { clientName : string;
+                          tabs       : string [];
+                          title      : string;
+                          urlHistory : string [];
+                          icon       : string;
+                          lastUsed   : StringOrInteger }
+
+        type Version2 = { clientID  : string;
+                          title     : string;
+                          history   : string [];
+                          lastUsed  : Int64; // Time in seconds since Unix epoch that tab was last active.
+                          icon      : string;
+                          groupName : string }
+    type Tabs = 
+    | Version1 of TabsVersions.Version1
+    | Version2 of TabsVersions.Version2
 
 // Utilities
 [<AutoOpen>]
@@ -125,7 +205,15 @@ module Utilities =
 
     let keepAsciiPrintableChars (x : string) =
         x.ToCharArray() |> Array.filter (fun x -> if int x < 32 || int x > 126 then false else true) |> fun x -> new string (x)
-    
+
+    // http://www.fssnip.net/3y
+    let getRecordFields (r: 'record) =
+        typeof<'record> |> Microsoft.FSharp.Reflection.FSharpType.GetRecordFields 
+        
+    let getRecordField (r: 'record) (field : Reflection.PropertyInfo) =
+        Microsoft.FSharp.Reflection.FSharpValue.GetRecordField(r,field) |> unbox
+        
+
     // Cryptography
 
     // E:\projects\fs-random-snippets>"%HOME%\Documents\Visual Studio 2012\Projects\Tutorial3\.nuget\nuget" PBKDF2.NET
@@ -260,6 +348,13 @@ module ServerAPI =
     let serverURL username = "https://auth.services.mozilla.com/user/1.0/" + username + "/node/weave"
     let clusterURL username = fetchUrlResponse (username |> serverURL) "GET" None None None None
 
+ 
+
+
+module CryptoKeys =
+    open Secrets
+    open Utilities
+
     let fetchCryptoKeys username password =
         let url = (clusterURL username) + "1.1/" + username + "/storage/crypto/keys"
         fetchUrlResponse url "GET" (Some (username, password)) None None None
@@ -270,7 +365,7 @@ module ServerAPI =
                                ciphertext : string
                                hmac       : string }
 
-    let writeSecretKeysToDisk username password (file : string option) = 
+    let writeCryptoKeysToDisk username password (file : string option) = 
         let secretKeys = fetchCryptoKeys username password
         let mutable file' = ""
         match file with 
@@ -280,16 +375,150 @@ module ServerAPI =
         stream.WriteLine(secretKeys)
         stream.Close()
 
-    let readSecretKeysFromDisk (file : string option) = 
+    let readCryptoKeysFromDisk (file : string option) = 
         try 
             match file with 
             | Some file -> file |> File.ReadAllText
             | _ -> Environment.GetEnvironmentVariable("HOME") + @"\.ssh\ff-secretKeys.json" |> File.ReadAllText
-        with | _ -> "" 
+        with | _ -> ""
+
+
+    let decryptCryptoKeys secrets cryptoKeys = 
+        let ck_pl = (cryptoKeys?payload).AsString() |> JsonValue.Parse
+
+        // https://docs.services.mozilla.com/sync/storageformat5.html
+        //
+        //    ciphertext  = record.ciphertext
+        //    iv          = record.iv
+        //    record_hmac = record.hmac
+        let record = { iv         = (ck_pl?IV).AsString()
+                       ciphertext = (ck_pl?ciphertext).AsString()
+                       hmac       = (ck_pl?hmac).AsString() }
+        //
+        //    encryption_key = bundle.encryption_key
+        //    hmac_key       = bundle.hmac_key
+        let bundle = syncKeyBundle secrets.username (secrets.encryptionpassphrase |> base32Decode)
+        //
+        //    local_hmac = HMACSHA256(hmac_key, base64(ciphertext))
+        let local_hmac = record.ciphertext |> Convert.FromBase64String |> (new HMACSHA256(bundle.hmac_key)).ComputeHash
+        //
+        //    if local_hmac != record_hmac:
+        //      throw Error("HMAC verification failed.")
+        //
+        //    cleartext = AESDecrypt(ciphertext, encryption_key, iv)
+        let cleartext = DecryptAES record.ciphertext bundle.encryption_key (record.iv |> Convert.FromBase64String)
+        cleartext
+
+
+    let getCryptoKeys secrets =
+        try
+            fetchCryptoKeys secrets.username secrets.password
+            |> JsonValue.Parse
+            |> decryptCryptoKeys secrets
+            |> keepAsciiPrintableChars
+            |> JsonValue.Parse 
+            |> fun x -> (x.GetProperty "default").AsArray()
+            |> Array.map (fun x -> x.AsString())
+            |> Array.map Convert.FromBase64String
+            |> Some
+        with 
+        | _ -> None
+
+module GeneralInfo = 
+    open Secrets
+    open ServerAPI
+    
+    let s = Secrets.secrets
+    let url = clusterURL s.username  
+
+    let fetchInfoCollections username password =
+        let url = (clusterURL username) + "1.1/" + username + "/info/collections"
+        fetchUrlResponse url "GET" (Some (username, password)) None None None
+
+    let fetchInfoQuota username password =
+        let url = (clusterURL username) + "1.1/" + username + "/info/quota"
+        fetchUrlResponse url "GET" (Some (username, password)) None None None
+
+    let fetchInfoCollectionUsage username password =
+        let url = (clusterURL username) + "1.1/" + username + "/info/collection_usage"
+        fetchUrlResponse url "GET" (Some (username, password)) None None None
+
+    let fetchInfoCollectionCounts username password =
+        let url = (clusterURL username) + "1.1/" + username + "/info/collection_counts"
+        fetchUrlResponse url "GET" (Some (username, password)) None None None
+
+    let deleteStorage username password =
+        let url = (clusterURL username) + "1.1/" + username
+        fetchUrlResponse url "GET" (Some (username, password)) None None None
+
+    let ic = fetchInfoCollections s.username s.password
+
+    let iq = fetchInfoQuota s.username s.password
+
+    let icu = fetchInfoCollectionUsage s.username s.password
+
+    let icc = fetchInfoCollectionCounts s.username s.password
+
+
+module Collections =
+    open Secrets
+    open ServerAPI
+    open CryptoKeys
+
+    let s = Secrets.secrets
+    let url = clusterURL s.username
+    
+    // https://github.com/mikerowehl/firefox-sync-client-php/blob/master/sync.php#L94
+    let fetchCollection username password collection =
+        let url = (clusterURL username) + "1.1/" + username + "/storage/" + collection + "?full=1"
+        fetchUrlResponse url "GET" (Some (username, password)) None None None
+
+    let bm' = fetchCollection s.username s.password "bookmarks"
+
+    let parseCollection collection = 
+        collection
+        |> JsonValue.Parse 
+        |> fun x -> x.AsArray() 
+        |> Array.map string 
+        |> Array.map (fun x -> JsonValue.Parse x) 
+        |> Array.map (fun x -> (x?payload).AsString())
+        |> Array.map (fun x -> JsonValue.Parse x)
+        |> Array.map (fun x -> { iv = x?IV.AsString(); ciphertext = x?ciphertext.AsString(); hmac = x?hmac.AsString() } )
+
+    let bm = parseCollection bm'
+    let cryptoKeysCleartext = 
+        readCryptoKeysFromDisk None 
+        |> JsonValue.Parse 
+        |> decryptCryptoKeys secrets
+    let key = 
+        cryptoKeysCleartext 
+        |> keepAsciiPrintableChars
+        |> JsonValue.Parse 
+        |> fun x -> (x.GetProperty "default").AsArray()
+        |> fun x -> x.[0].AsString()
+        |> Convert.FromBase64String
+    let cleartext = [|for b in bm do yield DecryptAES b.ciphertext key (b.iv |> Convert.FromBase64String) |]
+
+
+    let cleartext' = 
+        cleartext
+        |> Array.map keepAsciiPrintableChars 
+        |> Array.map JsonValue.Parse
+
+    let x = 
+        cleartext'.[0] 
+        |> fun x -> x.TryGetProperty "keyword"
+        |> fun x -> match x with | Some x -> x.AsString() | _ -> ""
+
+    let x' = 
+        cleartext'.[0] 
+        |> fun x -> x.TryGetProperty "parentName"
+        |> fun x -> match x with | Some x -> x.AsString() | _ -> ""
 
 
 module Test = 
     open Utilities
+    open CryptoKeys
 
     // base32Decode
 
@@ -334,88 +563,20 @@ module Test =
         failwith "syncKeyBundle failed"
 
     
-    // writeSecretKeysToDisk
+    // writeCryptoKeysToDisk
 
     try
-        writeSecretKeysToDisk secrets.username secrets.password None
-    with | _ -> failwith "writeSecretKeysToDisk failed"
+        writeCryptoKeysToDisk secrets.username secrets.password None
+    with | _ -> failwith "writeCryptoKeysToDisk failed"
 
 
-module CryptoKeys =
-    open Secrets
-    open Utilities
+    // getRecordFields/getRecordField
 
-    let sk = readSecretKeysFromDisk None |> JsonValue.Parse
-    let sk_pl = (sk?payload).AsString() |> JsonValue.Parse
+    let x = {value = "value"; name = Some "name"; ``type`` = Some "type"; }
 
-// https://docs.services.mozilla.com/sync/storageformat5.html
-//
-//    ciphertext  = record.ciphertext
-//    iv          = record.iv
-//    record_hmac = record.hmac
-    let record = { iv         = (sk_pl?IV).AsString()
-                   ciphertext = (sk_pl?ciphertext).AsString()
-                   hmac       = (sk_pl?hmac).AsString() }
-//
-//    encryption_key = bundle.encryption_key
-//    hmac_key       = bundle.hmac_key
-    let bundle = syncKeyBundle secrets.username (secrets.encryptionpassphrase |> base32Decode)
-//
-//    local_hmac = HMACSHA256(hmac_key, base64(ciphertext))
-    let local_hmac = record.ciphertext |> Convert.FromBase64String |> (new HMACSHA256(bundle.hmac_key)).ComputeHash
-//
-//    if local_hmac != record_hmac:
-//      throw Error("HMAC verification failed.")
-//
-//    cleartext = AESDecrypt(ciphertext, encryption_key, iv)
-    let cleartext = DecryptAES record.ciphertext bundle.encryption_key (record.iv |> Convert.FromBase64String)
+    let x' = getRecordFields x
 
-module Collections =
-    open Secrets
-    open ServerAPI
+    let x'' = { value = getRecordField x x'.[0]; name = getRecordField x x'.[1]; ``type`` = getRecordField x x'.[2] }
 
-    let s = Secrets.secrets
-    let url = clusterURL s.username
-    
-    // https://github.com/mikerowehl/firefox-sync-client-php/blob/master/sync.php#L94
-    let fetchCollection username password collection =
-        let url = (clusterURL username) + "1.1/" + username + "/storage/" + collection + "?full=1"
-        fetchUrlResponse url "GET" (Some (username, password)) None None None
-
-    let bm' = fetchCollection s.username s.password "bookmarks"
-
-    let parseCollection collection = 
-        collection
-        |> JsonValue.Parse 
-        |> fun x -> x.AsArray() 
-        |> Array.map string 
-        |> Array.map (fun x -> JsonValue.Parse x) 
-        |> Array.map (fun x -> (x?payload).AsString())
-        |> Array.map (fun x -> JsonValue.Parse x)
-        |> Array.map (fun x -> { iv = x?IV.AsString(); ciphertext = x?ciphertext.AsString(); hmac = x?hmac.AsString() } )
-
-    let bm = parseCollection bm'
-    let key = 
-        CryptoKeys.cleartext 
-        |> keepAsciiPrintableChars
-        |> JsonValue.Parse 
-        |> fun x -> (x.GetProperty "default").AsArray()
-        |> fun x -> x.[0].AsString()
-        |> Convert.FromBase64String
-    let cleartext = [|for b in bm do yield DecryptAES b.ciphertext key (b.iv |> Convert.FromBase64String) |]
-
-
-    let cleartext' = 
-        cleartext
-        |> Array.map keepAsciiPrintableChars 
-        |> Array.map JsonValue.Parse
-
-    let x = 
-        cleartext'.[0] 
-        |> fun x -> x.TryGetProperty "keyword"
-        |> fun x -> match x with | Some x -> x.AsString() | _ -> ""
-
-    let x' = 
-        cleartext'.[0] 
-        |> fun x -> x.TryGetProperty "parentName"
-        |> fun x -> match x with | Some x -> x.AsString() | _ -> ""
+    if x <> x'' then 
+        failwith "getRecordFields/getRecordField failed"
