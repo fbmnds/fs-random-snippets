33f0b4da442087efe0001ef37eb178960b5c5e91
fbmnds <fbmnds7@gmail.com>
2015-12-23 08:33:08 +0100

    Clean up

--- SharpGLTutorial.01.fsx
+++ SharpGLTutorial.01.fsx
@@ -38,7 +38,7 @@ open GlmNet
 
 
 /// Extending SharpGL.Shaders.ShaderProgram
-module ShaderProgram =
+module Base =
     
     /// Loads the shader sourcecode from the named manifest resource.
     let LoadManifestResource (textFileName : string) =
@@ -57,32 +57,35 @@ module ShaderProgram =
             File.ReadAllText(textFileName)
         else
             File.ReadAllText(Path.Combine(__SOURCE_DIRECTORY__, textFileName))
-    
+
+
+    let OpenGL (openGLVersion : Version.OpenGLVersion) 
+                (renderContextType : RenderContextType)
+                width height bitDepth 
+                (parameters : obj) =
+        let gl = OpenGL()
+        if not (gl.Create(openGLVersion, 
+                          renderContextType, 
+                          width, height, 32, 
+                          parameters)) then
+            failwith (sprintf "Base.glCreate SharpGL.Version.OpenGLVersion %A" openGLVersion)       
+        gl
+
+
+
+
+
+
+
 
 module Main = 
-    let vertices =
-        [| 0.75f;  0.75f; 0.0f; 1.0f;
-           0.75f; -0.75f; 0.0f; 1.0f;
-           -0.75f; -0.75f; 0.0f; 1.0f |]
 
-    let positionBufferObject = [|0u|]
 
-    let vao = [|0u|]
-    
-    let gl = OpenGL()
-    if not (gl.Create(SharpGL.Version.OpenGLVersion.OpenGL2_0, 
-                      SharpGL.RenderContextType.FBO, 
-                      800, 600, 32, 
-                      System.IntPtr.Zero)) then
-        failwith "gl.Create SharpGL.Version.OpenGLVersion.OpenGL3_1"
-
-    let toString s = s |> Array.reduce (fun x y -> sprintf "%s\n%s" x y) 
-
-#if DEBUG
+    // https://github.com/mattdesl/lwjgl-basics/wiki/GLSL-Versions
     let vertexShaderCode =
         """
-#version 310
-layout(location = 0) in vec4 Position;
+#version 140
+in vec4 Position;
 void main()
 {
 	gl_Position = Position;
@@ -91,88 +94,82 @@ void main()
 
     let fragmentShaderCode =
         """
-#version 310
+#version 140
 out vec4 outputColor;
 void main()
 {
-	outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	outputColor = vec4(0.f,0.6f,0.8f,0.5f);
 }
 """
-#else    
-    let vertexShaderCode =
-        [| "#version 110"
-           "vec4 Position;"
-           "void main()"
-           "{"
-           "    gl_Position = Position;"
-           "}" |] |> toString
 
-    let fragmentShaderCode =
-        [| "#version 110"
-           "vec4 outputColor;"
-           "void main()"
-           "{"
-           "    outputColor = vec4(0.1,0.2,0.3,0.4);"
-           "}" |] |> toString 
-#endif
+    let vertices =
+        [| 0.75f;  0.75f; 0.0f; 1.0f;
+           0.75f; -0.75f; 0.0f; 1.0f;
+           -0.75f; -0.75f; 0.0f; 1.0f |]
 
-    let attributeLocations = new Dictionary<uint32, string>()
-    let positionAttribute = 0u
-    let normalAttribute = 1u
-    let indexAttribute = 2u        
-    attributeLocations.Add (positionAttribute, "Position")
-    attributeLocations.Add (normalAttribute, "Normal")
-    attributeLocations.Add (indexAttribute, "outputColor")
+    let positionBufferObject = [|for i in [0u .. 2u] do yield 0u|]
 
-    let shaderProgram = ShaderProgram()
-    shaderProgram.Bind(gl)
-    attributeLocations.AsEnumerable()
-    |> Seq.iter (fun x -> shaderProgram.BindAttributeLocation(gl, x.Key, x.Value))
+    let vao = [|for i in [0u .. 2u] do yield 0u|]
     
-
-#if DEBUG
-    let shader gl shaderSource = 
-        let shader = Shaders.VertexShader()
-        shader.CreateInContext(gl)
-        shader.SetSource(shaderSource)
-        shader.Compile()
-        if not (shader.CompileStatus.HasValue) then 
-            failwith (sprintf "failed with source:\n %s" shaderSource)
-        if not (shader.CompileStatus.Value) then
-            failwith (sprintf "failed with source:\n%s\ninfoLog:\%s" shaderSource shader.InfoLog)
-        shader
-
-    let pixelVertexShader = shader gl vertexShaderCode
-    let pixelFragmentShader = shader gl fragmentShaderCode
-
-    pixelVertexShader.IsEnabled <- true
-    pixelFragmentShader.IsEnabled <- true
-#endif
-
-    shaderProgram.Create(gl, vertexShaderCode, fragmentShaderCode, attributeLocations)
+    let shaderProgram = ShaderProgram()
 
     /// Called after the window and OpenGL are initialized. Called exactly once, before the main loop.
-    let Init () =
-        gl.GenBuffers(1, positionBufferObject)
+    let Init (gl : OpenGL) =
 
-        gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, positionBufferObject.[0])
+        let attributeLocations = new Dictionary<uint32, string>()
+        let positionAttribute = 0u
+        let normalAttribute = 1u
+        let indexAttribute = 2u        
+        attributeLocations.Add (positionAttribute, "Position")
+        attributeLocations.Add (normalAttribute, "Normal")
+        attributeLocations.Add (indexAttribute, "outputColor")
+        
+        shaderProgram.Bind(gl)
+        attributeLocations.AsEnumerable()
+        |> Seq.iter (fun x -> shaderProgram.BindAttributeLocation(gl, x.Key, x.Value))
+    
+        let shader gl shaderSource = 
+            let shader = Shaders.VertexShader()
+            shader.CreateInContext(gl)
+            shader.SetSource(shaderSource)
+            shader.Compile()
+            if not (shader.CompileStatus.HasValue) then 
+                failwith (sprintf "failed with source:\n %s" shaderSource)
+            if not (shader.CompileStatus.Value) then
+                failwith (sprintf "failed with source:\n%s\ninfoLog:\n%s" shaderSource shader.InfoLog)
+            shader
+
+        let pixelVertexShader = shader gl vertexShaderCode
+        let pixelFragmentShader = shader gl fragmentShaderCode
+
+        pixelVertexShader.IsEnabled <- true
+        pixelFragmentShader.IsEnabled <- true
+    
+        gl.AttachShader (shaderProgram.ShaderProgramObject, pixelVertexShader.ShaderObject)
+        gl.AttachShader (shaderProgram.ShaderProgramObject, pixelFragmentShader.ShaderObject)
+        gl.LinkProgram (shaderProgram.ShaderProgramObject)
+        
+        gl.GenBuffers(3, positionBufferObject)
+
+        gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, 0u)//positionBufferObject.[0])
         gl.BufferData(OpenGL.GL_ARRAY_BUFFER, vertices, OpenGL.GL_STATIC_DRAW)
         gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, 0u)
 
-        gl.GenVertexArrays(1, vao)
+        gl.GenVertexArrays(3, vao)
         gl.BindVertexArray(vao.[0])
 
-    let Display () =
-        gl.ClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+    let Display (gl : OpenGL) =
+        gl.ClearColor (1.f,1.f,1.f,0.2f) //(0.f,0.6f,0.8f,0.5f)
         gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT)
+        //gl.Color (0.f,0.6f,0.8f,0.5f)
 
         gl.UseProgram(shaderProgram.ShaderProgramObject)
 
-        gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, positionBufferObject.[0])
+        gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, vao.[0])
         gl.EnableVertexAttribArray(0u)
-        gl.VertexAttribPointer(0u, 4, OpenGL.GL_FLOAT, OpenGL.GL_FALSE>0u, 0, IntPtr(0))
+        gl.VertexAttribPointer(0u, 4, OpenGL.GL_FLOAT, false, 0, IntPtr(int vao.[0]))
 
-        gl.DrawArrays(OpenGL.GL_TRIANGLES, 0, 3)
+        gl.DrawArrays(OpenGL.GL_TRIANGLES, (int vao.[0]), 3)
         gl.Flush()
 //        gl.DisableVertexAttribArray(0u)
 //        gl.UseProgram(0u)
@@ -185,10 +182,7 @@ void main()
     let ComboBox_Selected (sender : obj, x : EventArgs) = ()
     let CheckBox_Selected (sender : obj, x : EventArgs) = ()
 
-    let Run () =
-
-        let app = new Application()
-
+    let Window () =
         // Load the window.xaml file
         let w =
             @"fs-random-snippets\CelShadingExample\MainWindow.xaml"
@@ -201,30 +195,31 @@ void main()
         let checkBox = w.FindName("checkBoxUseToonShader") :?> CheckBox
         checkBox.Checked.Add (fun x -> CheckBox_Selected(checkBox,x))
 
-        //<sharpGL:OpenGLControl x:Name="openGlCtrl"
-        //OpenGLDraw="OpenGLControl_OpenGLDraw" OpenGLInitialized="OpenGLControl_OpenGLInitialized" 
-        //RenderContextType="FBO" Resized="OpenGLControl_Resized" />
-
         let mutable openGlCtrl =  w.FindName("openGlCtrl") :?> SharpGL.WPF.OpenGLControl
-        openGlCtrl.InitializeComponent()
-        ref (openGlCtrl.OpenGL) := gl
-
-        printfn "#0"
-        Init()
-        printfn "#1"
-        openGlCtrl.OpenGLDraw.Add (fun _ -> ())
-        printfn "#2"
-        openGlCtrl.OpenGLInitialized.Add (fun _ -> ())
-        printfn "#3"
-        openGlCtrl.RenderContextType <- RenderContextType.FBO
-        printfn "#4"
-        openGlCtrl.Resized.Add (fun _ -> ())
-        printfn "#5"
-        openGlCtrl.Visibility <- Visibility.Visible
-        printfn "#6"
-        Display()
-
-        w |> app.Run |> ignore
         
+        openGlCtrl.InitializeComponent()
+        
+        ref (openGlCtrl.OpenGL) := 
+            Base.OpenGL 
+                Version.OpenGLVersion.OpenGL3_1
+                RenderContextType.FBO
+                800 600 32
+                (box [||])
+        
+        openGlCtrl.OpenGLVersion <- Version.OpenGLVersion.OpenGL3_1
+        
+        [| 
+            fun _ -> ()
+            fun _ -> Init (openGlCtrl.OpenGL)                          
+            fun _ -> openGlCtrl.OpenGLDraw.Add (fun x -> Display (x.OpenGL)) 
+            fun _ -> openGlCtrl.OpenGLInitialized.Add (fun x ->  ())         
+            fun _ -> openGlCtrl.RenderContextType <- RenderContextType.FBO
+            fun _ -> openGlCtrl.Resized.Add (fun _ -> ())                    
+            fun _ -> openGlCtrl.Visibility <- Visibility.Visible            
+        |]
+        |> Array.iteri (fun i f -> f (); printfn "%d" i)
 
-Main.Run()
+        w.Title <- sprintf "%s - OpenGL %A" w.Title openGlCtrl.OpenGLVersion
+        w
+ 
+Main.Window () |> (Application ()).Run |> ignore
